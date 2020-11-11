1b6111e840fc9cf9245bbc48b43c2fc9a1ee0d32
Jack Tam <jiajie.tan@midea.com>
2020-11-12 00:30:07 +0800

    列出用户端接口所以接口

--- routes/customer.js
+++ routes/customer.js
@@ -17,4 +17,44 @@ router.get('/', async function(req, res, next) {
     }
 });
 
+// 顾客注册接口
+router.post('/register', async (req, res, next) => {
+    
+})
+
+// 顾客登录接口
+router.get('/login', async (req, res, next) => {
+
+})
+
+// 获取顾客信息接口
+router.get('/customer-info', async (req, res, next) => {
+
+})
+
+// 更新顾客信息接口
+router.post('/edit-customer-info', async (req, res, next) => {
+
+})
+
+// 更新顾客密码接口
+router.post('/update-customer-password', async (req, res, next) => {
+
+})
+
+// 获取收件信息接口
+router.get('/get-address-list', async (req, res, next) => {
+
+})
+
+// 获取新增收件信息接口
+router.post('/create-address', async (req, res, next) => {
+
+})
+
+// 获取更新收件信息接口
+router.post('/update-address', async (req, res, next) => {
+
+})
+
 module.exports = router;
