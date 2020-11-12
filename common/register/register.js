440a54cce557abdf5cc0e69bd211f742de0c326c
Jack Tam <jiajie.tan@midea.com>
2020-11-13 00:27:14 +0800

    注册

---
+++ common/register/register.js
@@ -0,0 +1,13 @@
+var db = require('../../db/db');
+
+const register = {
+    createUser() {
+        // 新建用户
+
+    },
+    checkUserAlreadyRegistered() {
+        // 检查用户是否已经注册
+    }
+}
+
+module.exports = register;
