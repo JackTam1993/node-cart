aac8ef64d7fce03eca02ee69739e699a8a3e086a
Jack Tam <jiajie.tan@midea.com>
2020-11-22 16:39:50 +0800

    增加启动脚本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   apps : [{
     name: "app",
-    script: "./app.js",
-    exec_mode : "cluster",
+    script: "./bin/www",
+    exec_mode : "fork",
+    watch: true,
     env: {
       "PORT": 3000,
       NODE_ENV: "development",
