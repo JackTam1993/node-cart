3e2c18d7e17e4d3af322d7714bdd3ad07ab3d31d
Jack Tam <jiajie.tan@midea.com>
2020-11-25 20:00:04 +0800

    增加跨域头

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 app.use('/customer', customerRouter);
 app.use('/staff', staffRouter);
 
