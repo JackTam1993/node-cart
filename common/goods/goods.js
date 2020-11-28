3b7d803bd4ba7f267996f518a372e23d41cf17fe
Jack Tam <jiajie.tan@midea.com>
2020-11-28 23:03:28 +0800

    获取商品详情增加price

--- common/goods/goods.js
+++ common/goods/goods.js
@@ -21,7 +21,7 @@ const goods = {
     },
     async getItemDetail(product_id) {
         try {
-            let result = await db.query(`SELECT * FROM product LEFT JOIN product_detail ON product.product_id = product_detail.product_id WHERE product.product_id = ${product_id}`);
+            let result = await db.query(`SELECT * FROM product LEFT JOIN product_detail ON product.product_id = product_detail.product_id right join product_price on product.product_id = product_price.product_id WHERE product.product_id = ${product_id}`);
 
             return result.rows[0];
         } catch (error) {
