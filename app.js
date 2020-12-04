ccddf6a26f36149ccf261c11f86f9b6d60f9fd06
tanjj4 <jiajie.tan@midea.com>
2020-12-04 17:48:06 +0800

    增加静态文件目录

--- app.js
+++ app.js
@@ -23,12 +23,15 @@ app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,token");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Content-Type", "application/json;charset=utf-8");
+  // res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
 
 app.use('/customer', customerRouter);
 app.use('/staff', staffRouter);
+
+// 托管静态文件
+app.use('/static', express.static(path.join(__dirname, 'static')))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- common/stock/stock.js
+++ common/stock/stock.js
@@ -4,7 +4,7 @@ const stock = {
     async getAllStock() {
         // 获取全部商品
         try {
-            let result = await db.query(`select * from stock s LEFT JOIN warehouse w ON s.warehouse_id = w.warehouse_id RIGHT JOIN product p ON s.product_id = p.product_id WHERE s.product_id IS not NULL`);
+            let result = await db.query(`select * from stock s LEFT JOIN warehouse w ON s.warehouse_id = w.warehouse_id RIGHT JOIN product p ON s.product_id = p.product_id RIGHT JOIN product_price pp ON s.product_id = pp.product_id RIGHT JOIN product_detail pd ON s.product_id = pd.product_id WHERE s.product_id IS not NULL`);
 
             return result.rows;
         } catch (error) {
@@ -14,7 +14,7 @@ const stock = {
     async getStockByWarehouseId(warehouse_id) {
         // 获取某个仓库下的全部商品
         try {
-            let result = await db.query(`select * from stock s LEFT JOIN warehouse w ON s.warehouse_id = w.warehouse_id RIGHT JOIN product p ON s.product_id = p.product_id WHERE s.product_id IS not NULL and warehouse_id = ${warehouse_id}`);
+            let result = await db.query(`select * from stock s LEFT JOIN warehouse w ON s.warehouse_id = w.warehouse_id RIGHT JOIN product p ON s.product_id = p.product_id RIGHT JOIN product_price pp ON s.product_id = pp.product_id RIGHT JOIN product_detail pd ON s.product_id = pd.product_id WHERE s.product_id IS not NULL and warehouse_id = ${warehouse_id}`);
 
             return result.rows;
         } catch (error) {
